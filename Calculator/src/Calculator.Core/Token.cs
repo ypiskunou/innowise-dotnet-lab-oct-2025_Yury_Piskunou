745d51df1fa4bcdbd0e6b2b292fcfe8fb4c573fe
Yury Piskunou <ypiskunou@gmail.com>
2025-10-03 23:21:50 +0300

    feat(core): Define core interfaces and models

--- Calculator/src/Calculator.Core/Token.cs
+++ Calculator/src/Calculator.Core/Token.cs
@@ -1,16 +1,3 @@
 namespace Calculator.Core;
 
-public class Token
-{
-    public TokenType Type { get; }
-    public string Lexeme { get; } // Исходная строка, например "+", "123", "("
-    public double? Value { get; } // Только для чисел
-
-    // Конструктор, чтобы сделать объект неизменяемым (immutable)
-    public Token(TokenType type, string lexeme, double? value = null)
-    {
-        Type = type;
-        Lexeme = lexeme;
-        Value = value;
-    }
-}
+public record Token(TokenType Type, string Lexeme, double? Value = null);
