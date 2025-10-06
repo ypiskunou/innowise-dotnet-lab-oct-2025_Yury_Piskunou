939ce440a7ef35f0796665862acf820f38765647
Yury Piskunou <ypiskunou@gmail.com>
2025-10-06 19:27:31 +0300

    feat(engine): Implement Undo functionality using Command pattern

--- Calculator/src/Calculator.Engine/CalculatorEngine.cs
+++ Calculator/src/Calculator.Engine/CalculatorEngine.cs
@@ -1,13 +1,16 @@
 namespace Calculator.Engine;
 
 using Core;
+using System.Collections.Generic;
 
 public class CalculatorEngine : ICalculatorSession
 {
     private readonly ICalculationService _calculationService;
     
+    private readonly Stack<ICommand> _history = new Stack<ICommand>();
+
     public double CurrentValue { get; private set; }
-    
+
     public CalculatorEngine(ICalculationService calculationService)
     {
         _calculationService = calculationService;
@@ -16,13 +19,23 @@ public class CalculatorEngine : ICalculatorSession
 
     public void ExecuteExpression(string expression)
     {
-        var result = _calculationService.Evaluate(expression);
+        var previousValue = CurrentValue;
         
+        var result = _calculationService.Evaluate(expression);
         CurrentValue = result;
+
+        var command = new CalculationCommand(previousValue, result);
+        
+        _history.Push(command);
     }
 
     public void UndoLast()
     {
-        throw new NotImplementedException();
+        if (_history.Count > 0)
+        {
+            var command = _history.Pop();
+            
+            CurrentValue = command.UnExecute();
+        }
     }
 }
